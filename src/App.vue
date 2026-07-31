430af32cc81b32ea4e2ab77a61881a85a5e9b413
bmh7190 <bmh7190@naver.com>
2026-07-31 15:20:51 +0900

    feat: Vue Directive
    - v-html / v-html XSS / v-text
    - v-bind (Basic, Class Binding, Style Binding, Shorthand)
    - v-for
    - v-pre / v-cloak / v-once / v-memo

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,42 @@
 <script setup>
 import SampleOne from './components/practices/basic/SampleOne.vue'
 import SampleTwo from './components/practices/basic/SampleTwo.vue'
+import VueHtmlXss from './components/practices/basic/VueHtmlXss.vue'
+import VueHTML from './components/practices/basic/VueHTML.vue'
+import VueText from './components/practices/basic/VueText.vue'
+import VueBind from './components/practices/basic/VueBind.vue'
+import VueBindClass from './components/practices/basic/VueBindClass.vue'
+import VueBindStyle from './components/practices/basic/VueBindStyle.vue'
+import VueBindShorthand from './components/practices/basic/VueBindShorthand.vue'
+import VueIf from './components/practices/basic/VueIf.vue'
+import VueShow from './components/practices/basic/VueShow.vue'
+import VueFor from './components/practices/basic/VueFor.vue'
+import VueMemo from './components/practices/basic/VueMemo.vue'
+import VueOnce from './components/practices/basic/VueOnce.vue'
+import VuePre from './components/practices/basic/VuePre.vue'
+import VueCloak from './components/practices/basic/VueCloak.vue'
 </script>
 
 <template>
-  <SampleOne />
+  <div>
+    <SampleOne />
+  </div>
+
   <SampleTwo />
+  <VueHtmlXss />
+  <VueHTML />
+  <VueText />
+  <VueBind />
+  <VueBindClass />
+  <VueBindStyle />
+  <VueBindShorthand />
+  <VueIf />
+  <VueShow />
+  <VueFor />
+  <VuePre />
+  <VueCloak />
+  <VueOnce />
+  <VueMemo />
 </template>
 
 <style scoped></style>
