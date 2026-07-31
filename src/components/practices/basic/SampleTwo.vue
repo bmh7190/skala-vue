7b8f6586dea7634da3dcd31a0ae2d9e683c04634
bmh7190 <bmh7190@naver.com>
2026-07-31 11:56:28 +0900

    feat: 학습환경 구성
    - 반응성 데이터 (Reactivity) Example
    - JavaScript in Text Interpolation Example

---
+++ src/components/practices/basic/SampleTwo.vue
@@ -0,0 +1,11 @@
+<script setup>
+const welcomeMesage = 'Welcom to Skala Vue'
+</script>
+
+<template>
+  <div class="practice-section">
+    <h2>{{ welcomeMesage }}</h2>
+    <p>{{ welcomeMesage.toUpperCase() }}</p>
+    <p>{{ 'Random number: ' + Math.ceil(Math.random() * 100) }}</p>
+  </div>
+</template>
