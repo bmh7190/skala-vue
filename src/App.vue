54ae7185576767851756efed65f2257878840e01
bmh7190 <bmh7190@naver.com>
2026-08-03 11:46:35 +0900

    feat: Computed & Watchers
    - computed() Example
    - watch() Example
    - wach() Multi Source Watch Example
    - watch() Deep Watch Example
    - watch() reactive 반응형 데이터 Example
    - watchEffect() Example

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <script setup>
+import ComputedBasic from './components/practices/composition/ComputedBasic.vue'
 import ReactiveReactive from './components/practices/composition/ReactiveReactive.vue'
 import ReactiveRef from './components/practices/composition/ReactiveRef.vue'
+import WatchersBasic from './components/practices/composition/WatchersBasic.vue'
+import WatchersDeep from './components/practices/composition/WatchersDeep.vue'
+import WatchersMulti from './components/practices/composition/WatchersMulti.vue'
+import WatchersReactive from './components/practices/composition/WatchersReactive.vue'
+import WatchersWatchEffect from './components/practices/composition/WatchersWatchEffect.vue'
 
 // 1. Reactive State
 // 2. Computed & Watchers
@@ -14,7 +20,12 @@ import ReactiveRef from './components/practices/composition/ReactiveRef.vue'
     <ReactiveReactive />
     <h1>2. Computed & Watchers</h1>
     <hr />
-
+    <ComputedBasic />
+    <WatchersBasic />
+    <WatchersMulti />
+    <WatchersDeep />
+    <WatchersReactive />
+    <WatchersWatchEffect />
     <h1>3. Component Lifecycle</h1>
     <hr />
 
