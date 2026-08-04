3902a9f0204b2852c2ce7366992817fea716c57c
bmh7190 <bmh7190@naver.com>
2026-08-04 15:44:40 +0900

    feat: Axios Code Challenge
    - Axios Weather Example
    - Axios JSON Example

--- src/views/PracticeDay3View.vue
+++ src/views/PracticeDay3View.vue
@@ -1,4 +1,6 @@
 <script setup>
+import AxiosJson from '@/components/practices/library/AxiosJson.vue'
+import AxiosWeather from '@/components/practices/library/AxiosWeather.vue'
 import StoreCounter from '@/components/practices/library/StoreCounter.vue'
 </script>
 
@@ -7,5 +9,10 @@ import StoreCounter from '@/components/practices/library/StoreCounter.vue'
     <h1>1. Pinia</h1>
     <hr />
     <StoreCounter />
+
+    <h1>2. Axios</h1>
+    <hr />
+    <AxiosWeather />
+    <AxiosJson />
   </div>
 </template>
